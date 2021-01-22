a1498a2b51d013875b0ae8cfc6ada8fbf85faad5
FantastikN <necipaga@gmail.com>
2021-01-22 16:50:17 +0200

    Initial Commit

---
+++ app/src/main/java/gr/uom/project2020_smnaggregator/MainActivity.java
@@ -0,0 +1,14 @@
+package gr.uom.project2020_smnaggregator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
